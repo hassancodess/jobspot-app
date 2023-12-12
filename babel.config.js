c7aea3a92ba379395f43be786411caca1701308f
hassancodess <dev.hassancodes@gmail.com>
2023-12-12 22:53:10 +0500

    feat(config): setup path alias & multiple environment support

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,40 @@
 module.exports = function (api) {
-  api.cache(true);
-  const plugins = [];
+	api.cache(true);
+	const plugins = [];
 
-  plugins.push([
-    '@tamagui/babel-plugin',
-    {
-      components: ['tamagui'],
-      config: './tamagui.config.ts',
-    },
-  ]);
+	plugins.push([
+		'@tamagui/babel-plugin',
+		{
+			components: ['tamagui'],
+			config: './tamagui.config.ts',
+		},
+	]);
 
-  plugins.push('expo-router/babel');
+	plugins.push('expo-router/babel');
 
-  return {
-    presets: ['babel-preset-expo'],
-    plugins,
-  };
+	plugins.push([
+		'module-resolver',
+		{
+			root: ['./'],
+			alias: {
+				'@env': 'src/utils/env.js',
+			},
+			extensions: [
+				'.ios.ts',
+				'.android.ts',
+				'.ts',
+				'.ios.tsx',
+				'.android.tsx',
+				'.tsx',
+				'.jsx',
+				'.js',
+				'.json',
+			],
+		},
+	]);
+
+	return {
+		presets: ['babel-preset-expo'],
+		plugins,
+	};
 };
